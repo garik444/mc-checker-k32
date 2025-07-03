f06a278f17dd0a99ac100687d2a87fbc779572f1
garik444 <gghevondyan21@gmail.com>
2025-07-03 19:05:53 +0400

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function checkMC() {
   }
 
   const trucks = Math.floor(Math.random() * 50) + 1;
-  const drivers = Math.floor(Math.random() * (trucks + 1)); // ≤ trucks
-  const inspections = Math.floor(Math.random() * 20) + 1;
+  const drivers = Math.floor(Math.random() * trucks) + 1;
+  const inspections = Math.floor(Math.random() * 100) + 1;
 
   mcNumber.textContent = mcInput;
   truckCount.textContent = trucks;
